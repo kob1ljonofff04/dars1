f2fa3cf1823662f144165eedc0d916cf9d396c03
kob1ljonofff04 <125870986+kob1ljonofff04@users.noreply.github.com>
2023-02-21 09:40:48 +0500

    3 dars vazifasi

---
+++ 3 dars vazifalari.py
@@ -0,0 +1,113 @@
+'''
+3/1
+'''
+'''
+a = float(input("1-soni kiriting:"))
+b = float(input("2-soni kiriting:"))
+
+if a%2== 0 and  b%2 == 0:
+ print ("ikkalasiyam juft:")
+if a%2== 1 and b%2 == 0:
+    print("ikkalasiyam toq:")
+'''
+'''
+3/2
+'''
+'''
+qayiq_1 = float(input("
+3/3
+'''
+'''
+r = float(input("doira radiusini kiriting:"))
+print(3.14*(r**2))
+print(2*3.14*r)
+'''
+'''
+3/4
+'''
+'''
+a = float(input("v(km/soat) kiriting:"))
+b = float(input("bosib o'tgan s(km)ni kiriting:"))
+print ("s=v*t:")
+'''
+'''
+3/5
+'''
+'''
+a=float(input("radiusni kiriting:"))
+print(2*3.14*a)
+print(3.14*(a**2)
+'''
+'''
+3/6
+'''
+'''
+a=float(input("son kiriting:")
+print(       )
+'''
+'''
+3/7
+'''
+
+'''
+a=float(input("enini kiriting:"))
+b=float(input("boyi kiriting:"))         
+h=float(input("balandligi kiriting:"))         
+s=a*b +2*a*h +2*h*b
+v = a*b*h
+print (s, v)
+'''
+'''
+3/8
+'''
+'''
+1 soat = 3600 sekund
+1 min = 60 sekund
+1 soat = 60 minut
+'''
+'''
+a = float(input("sekundni kiriting"))
+soat = a//3600
+minut = (a%3600)//60
+sekund =(a%3600)%60
+print(soat,"soat", minut,"minut", sekund,"sekund")
+'''
+'''
+3/11
+'''
+'''
+ism = input("ismingizni kiriting:")
+son = int(input("son kiriting:"))
+print(ism*son)
+'''
+'''
+3/12
+'''
+'''
+ism = input("ism kiriting:")
+sinf =int(input("sinf kiritng:"))
+print ("men 9 sinf oquvchisi adham " )
+'''
+'''
+3/13
+'''
+'''
+a =input("ozgaruvchini kiriting:")
+b =input("ozgaruvchini kiriting:")
+c =input("ozgaruvchini kiriting:")
+d =input("ozgaruvchini kiriting:")
+e =input("ozgaruvchini kiriting:")
+print("")
+'''
+'''
+3/14
+'''
+'''
+a=float(input("ismini kiriting and sonini kiriting:")
+b=float(input("ismini kiriting and  sonini kiriting :")
+c=float(input("ismini kiriting and sonini kiriting:")
+print ("")
+'''
+'''
+
+        
